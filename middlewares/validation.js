eea8e9901e77759c845fc5a547eace7503b4279a
DmitryStarov <314263@bk.ru>
2023-08-11 23:50:02 +0800

    feat/middlewares

---
+++ middlewares/validation.js
@@ -0,0 +1,53 @@
+const { celebrate, Joi } = require('celebrate');
+const { REG_URL, REG_ID } = require('../utils/constants');
+
+const validatePostUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+});
+
+const validatePathUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+  }),
+});
+const validateLogin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+});
+
+const validatePostMovie = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().regex(REG_URL),
+    trailerLink: Joi.string().required().regex(REG_URL),
+    thumbnail: Joi.string().required().regex(REG_URL),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+});
+
+const validateDeleteMovie = celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().required().regex(REG_ID),
+  }),
+});
+
+module.exports = {
+  validatePostUser,
+  validatePathUser,
+  validateLogin,
+  validatePostMovie,
+  validateDeleteMovie,
+};
