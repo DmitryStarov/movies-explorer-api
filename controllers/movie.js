a12683866cfb530a3ff0e2ed0cc78addbcf02067
DmitryStarov <314263@bk.ru>
2023-08-11 23:09:56 +0800

    feat/movie controller

---
+++ controllers/movie.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose');
+const Movie = require('../models/movie');
+
+const {
+  CREATED_STATUS,
+  INVALID_ADD_CARD_MESSAGE,
+  CARD_NOT_FOUND_MESSAGE,
+  FORBIDDEN_DELETE_CARD_MESSAGE,
+} = require('../utils/constants');
+const BadRequest = require('../errors/BadRequest');
+const NotFound = require('../errors/NotFound');
+const Forbidden = require('../errors/Forbidden');
+
+module.exports.getMovies = (req, res, next) => {
+  Movie
+    .find({})
+    .populate(['owner', 'likes'])
+    .then((movies) => res.send(movies.reverse()))
+    .catch(next);
+};
+module.exports.postMovie = (req, res, next) => {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
+  } = req.body;
+  Movie
+    .create({
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailerLink,
+      thumbnail,
+      movieId,
+      nameRU,
+      nameEN,
+      owner: req.user._id,
+    })
+    .then((movie) => {
+      movie
+        .populate('owner')
+        .then(() => res.status(CREATED_STATUS).send(movie))
+        .catch(next);
+    })
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        return next(new BadRequest(INVALID_ADD_CARD_MESSAGE));
+      }
+      return next(err);
+    });
+};
+module.exports.deleteMovie = (req, res, next) => {
+  const { movieId } = req.params;
+  Movie
+    .findById(movieId)
+    .populate('owner')
+    .then((movie) => {
+      if (!movie) {
+        return next(new NotFound(CARD_NOT_FOUND_MESSAGE));
+      }
+      if (!movie.owner.equals(req.user._id)) {
+        return next(new Forbidden(FORBIDDEN_DELETE_CARD_MESSAGE));
+      }
+      return Movie.deleteOne(movie)
+        .then(() => {
+          res.send(movie);
+        })
+        .catch(next);
+    })
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        return next(new BadRequest(CARD_NOT_FOUND_MESSAGE));
+      }
+      return next(err);
+    });
+};
